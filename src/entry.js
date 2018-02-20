3b7b734690730bcfc73286da76ee234eed4e1ff4
Diego Alvarado <diego23k@gmail.com>
2018-02-20 22:28:50 +0100

    commit 2

--- src/entry.js
+++ src/entry.js
@@ -1,10 +1,2 @@
-import moment from 'moment';
-
-
 import css from './scss/style.scss';
 
-console.log("Hola amigos 24!!", moment().format());
-
-document.querySelector("body").addEventListener("click", (event) => {
-    console.log("Hello!");
-})
